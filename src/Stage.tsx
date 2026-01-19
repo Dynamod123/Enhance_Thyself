036816c16a675bf4961feb8033b1587d06c574cf
Dynamod <Amosda2019@gmail.com>
2026-01-19 17:00:36 -0600

    fix: Break brace sequences to robustly prevent template errors (Original Pipeline)

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -275,9 +275,10 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
 
     enhance(charId: string, userId: string, newHistory: string, targetContext: string, instructions: string = '') {
         // Sanitize inputs to prevent template parsing errors if they contain '{{'
-        const personality = (this.characters[charId]?.personality ?? '').replace(/{{/g, '\\{{');
-        const description = (this.characters[charId]?.description ?? '').replace(/{{/g, '\\{{');
-        const userProfile = (this.users[userId]?.chatProfile ?? '').replace(/{{/g, '\\{{');
+        // We break the braces apart because standard escaping (\\{{) might not be supported by the engine.
+        const personality = (this.characters[charId]?.personality ?? '').replace(/{{/g, '{ {').replace(/}}/g, '} }');
+        const description = (this.characters[charId]?.description ?? '').replace(/{{/g, '{ {').replace(/}}/g, '} }');
+        const userProfile = (this.users[userId]?.chatProfile ?? '').replace(/{{/g, '{ {').replace(/}}/g, '} }');
 
         return this.generator.textGen({
             prompt:
